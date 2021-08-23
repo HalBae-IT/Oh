97b874e2b2835ad4d762399c4d0618c11ad32536
Oh <87973076+OhTaeS2@users.noreply.github.com>
2021-08-23 13:45:26 +0900

    add: code 08

--- src/Hello.java
+++ src/Hello.java
@@ -1,22 +1,104 @@
+package Sectionl;
+
+import sun.security.util.math.intpoly.IntegerPolynomial;
+
 import java.util.Scanner;
 
-    class code08 {
-        public static  void main(String[] args) {
+public class Code08 {
 
-            Scanner kb = new Scanner(System.in);
-            int n = kb.nextInt();
-            int [] data = new int[n];
+    static Polynomial [] poLys = new Polynomial [100];
+    static int n = 0;
 
-            for(int i=0; i<n; i++)
-                data[i] = kb.nextInt();
-            kb.close();
+    public static void main(String[] args) {
 
-            int sum = 0, max = data[0];
-            for (int i=0; i<n;i++) {
-                sum += data[i];
-                if (data[i]>max) max = data[i];
+        Scanner kb = new Scanner(System.in);
+        while (ture) {
+            System.out.print("$ ");
+            String command = kb.next();
+            if (command.equals("create")) {
+                char name = kb.next().charAt(0);
+                poLys(n) = new Polynomial();
+                poLys(n).name = name;
+                poLys(n).terms = new Term [100];
+                poLys(n).nTerms = 0;
+                n++;
             }
-            System.out.println("The sum is" + sum +" and the maximum is" + max);
-    }
+            else if (command.equals("add")) {
+                char name = kb.next().charAt(0);
+                int index = find(name);
+                if (index == -1) {
+                    System.out.println("No such polynomial exists.");
+                }
+                else {
+                    int c = kb.nextInt();
+                    int e = kb.nextInt();
+                    addTerm( poLys[index], c, e);
+                }
+            }
+            else if (command.equals("calc")) {
+            }
+            else if (command.equals("print")) {
+                char name = kb.next().charAt(0);
+                int index = find(name);
+                if (index == -1) {
+                    System.out.println("No such polynomial exists.");
+                }
+                else {
+                    printPolynomal( polys[index] );
+                }
+            }
+            else if (command.equals("exit"))
+                break;
+        }
+            kb.close();
+        }
 
-}
+        private static int calcPolynomal(Polynimial p, int x){
+            int result = 0;
+            for (int i = 0; i<p.nTerms; i++)
+                result += colcTerm( p. terms[i], x);
+            return result;
+        }
+        private static int colcTerm(Term terms, int x){
+            return (int) (term.coef * Math.pow( x, term.expo ));
+        }
+
+        private static void printPolynomal(polynomial p) {
+            for (int i = 0; i < p.terms[1]; i++) ;
+            {
+                printTerm(p.terms[1]);
+                System.out.print("+");
+            }
+            System.out.println();
+        }
+        private static void printTerm(Term term) {
+            System.out.print(term.coef + "x^" + term.expo);
+        }
+        private static void addTerm(Polynomial p, int c, int e) {
+            int index = findTerm( p, e);
+            if (index != -1){
+                p.terms[index].coef += c;
+            }
+            else{
+                int i = p.nTerms-1;
+                while (i >=0 && p.terms[i].expo < e) {
+                    p.terms[i+1] = p.terms[i];
+                    i--;
+                }
+                p.terms[i+1] = new Term();
+                p.terms[i+1].coef = c;
+                p.terms[i+1].expo = e;
+                p.nTerms++;
+            }
+        }
+        private static int findTerm(Polynomial p, int e) {
+            for (int i = 0; i < p.nTerms && p.terms[i].expo >= e; i++)
+                return i;
+            return -1;
+        }
+        private static int find(char name) {
+            for (int i=0; i<n; i++)
+                if (poLys[i].name == name)
+                    return i;
+        }
+    }
